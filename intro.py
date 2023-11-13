dfe21feff857ed583c646ea973bb2c1d3402ce7e
Jean Duckens SANNON <jeanduckens.sanon@esih.edu>
2023-11-13 15:42:16 -0500

    Implementation of the code by Duckens

--- intro.py
+++ intro.py
@@ -1,1 +1,30 @@
-print("yes")
+from passwordgenerator import passwordgenerator as generate_password
+
+
+question = input("Do you want a short password? (y/n) ")
+if question == 'y':
+    try:
+        length = int(input("Enter the lenght for a short password: "))
+        short_password = generate_password.generate_password(length)
+        print(f"Short password: {short_password}")
+    except ValueError:
+        print("You must enter a number")
+else:
+    question = input("Do you want a strong password? (y/n) ")
+    if question == 'y':
+        try:
+            length = int(input("Enter the lenght for a strong password: "))
+            strong_password = generate_password.generate_password(length, uppercase=True, digits=True, special_chars=True)
+            print(f"Strong password: {strong_password}")
+        except ValueError:
+            print("You must enter a number")
+    else:
+        print("You must enter a valid answer")
+        exit()
+        
+
+
+
+
+
+
